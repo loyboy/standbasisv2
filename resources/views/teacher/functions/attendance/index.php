424e89d2cfb3d32b3ab2ed95c89c81302b4d1812
Emma Loy <loyboy606@gmail.com>
2019-12-03 16:37:50 +0100

    december commit

--- resources/views/teacher/functions/attendance/index.php
+++ resources/views/teacher/functions/attendance/index.php
@@ -5,7 +5,7 @@ function getSubTime($date, $period, $classid, $sch){
     $value = DB::select("SELECT a._done, s2.name FROM attendances a INNER JOIN subjectclasses s ON a.sub_class_id = s.id INNER JOIN timetables t ON a.time_id = t.id INNER JOIN subjects s2 ON s2.id = s.sub_id INNER JOIN class_streams c ON c.id = s.class_id WHERE a._date LIKE :dat AND t._time = :per AND t.school_id = :sch AND c.id = :cls" , [ "dat" => "%".$date."%", "per" => $period, "sch" => $sch, "cls" => $classid ] ); //get maximum value
    
     $done = intval($value[0]->_done) === 0 ? "<b style='color: red;'> NO </b>" : "<b style='color: green;'> YES </b>";
-    $subject - $value[0]->name;
+    $subject = $value[0]->name;
     $html = "<span> $done </span> <br> <span style='font-size: 9px; font-style: italic;'> $subject </span> ";
     return $html;
 }
