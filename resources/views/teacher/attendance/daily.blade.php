d633e6c7896ebe5119d85d4156d6f2fd0cd32611
Emma Loy <loyboy606@gmail.com>
2019-12-03 17:21:20 +0100

    december commit

--- resources/views/teacher/attendance/daily.blade.php
+++ resources/views/teacher/attendance/daily.blade.php
@@ -94,6 +94,32 @@
 		<td colspan="2"><?php echo getSubTime($date, '13:20:00',  5, 1); ?></td>
 		<td colspan="2"><?php echo getSubTime($date, '14:00:00',  5, 1); ?></td>
 	</tr>
+
+	<tr>
+		<td><b style="font-size: 18px;"> JS3B </b></td>
+		<td colspan="2"> <?php echo getSubTime($date, '8:05:00', 6, 1); ?></td>
+		<td colspan="2"> <?php echo getSubTime($date, '8:45:00', 6, 1); ?></td>
+		<td colspan="2"><?php echo getSubTime($date, '9:25:00',  6, 1); ?></td>
+		<td colspan="2"><?php echo getSubTime($date, '10:05:00', 6, 1); ?></td>
+		<td colspan="2"><?php echo getSubTime($date, '10:45:00',  6, 1); ?></td>
+		<td colspan="2"><?php echo getSubTime($date, '12:00:00',  6, 1); ?></td>
+		<td colspan="2"><?php echo getSubTime($date, '12:40:00',  6, 1); ?></td>
+		<td colspan="2"><?php echo getSubTime($date, '13:20:00',  6, 1); ?></td>
+		<td colspan="2"><?php echo getSubTime($date, '14:00:00',  6, 1); ?></td>
+	</tr>
+
+	<tr>
+		<td><b style="font-size: 18px;"> SS1A </b></td>
+		<td colspan="2"> <?php echo getSubTime($date, '8:05:00', 7, 1); ?></td>
+		<td colspan="2"> <?php echo getSubTime($date, '8:45:00', 7, 1); ?></td>
+		<td colspan="2"><?php echo getSubTime($date, '9:25:00',  7, 1); ?></td>
+		<td colspan="2"><?php echo getSubTime($date, '10:05:00', 7, 1); ?></td>
+		<td colspan="2"><?php echo getSubTime($date, '10:45:00',  7, 1); ?></td>
+		<td colspan="2"><?php echo getSubTime($date, '12:00:00',  7, 1); ?></td>
+		<td colspan="2"><?php echo getSubTime($date, '12:40:00',  7, 1); ?></td>
+		<td colspan="2"><?php echo getSubTime($date, '13:20:00',  7, 1); ?></td>
+		<td colspan="2"><?php echo getSubTime($date, '14:00:00',  7, 1); ?></td>
+	</tr>
 	
 </table>
 
