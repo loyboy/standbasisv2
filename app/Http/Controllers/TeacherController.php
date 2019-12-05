40f97607d65ddafd14e7234dda207a9cc84e83f6
Emma Loy <loyboy606@gmail.com>
2019-12-05 14:09:11 +0100

    decmeber 5th

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Database\Eloquent\Builder;
 use App\Teacher;
 use App\Subjectclass;
 
