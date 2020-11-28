069932f4149fd8ad7219475d303e3408d9535760
loyboy <loyboy606@gmail.com>
2020-11-28 19:55:55 +0100

    mades changes on 28th

--- app/Http/Controllers/MneController.php
+++ app/Http/Controllers/MneController.php
@@ -703,6 +703,9 @@ private function TgetTypeLsnManagementS($tea, $d, $d2, $term, $type){
          $k++;
      }  
      $i = 0;
+
+       $sch = $this->getschoolId( $tea );
+
        foreach ($sub as $s1) {
         
           foreach ($s1 as $s2 => $vd){    
