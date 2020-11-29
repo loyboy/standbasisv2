76e4dde13042f077c598970fe5ddc15348b7e564
loyboy <loyboy606@gmail.com>
2020-11-29 19:55:21 +0100

    made changes on 29th

---
+++ resources/views/modals/classm.blade.php
@@ -0,0 +1,68 @@
+<!-- View MNE Student page -->
+<div class="modal fade" id="getmneclass" tabindex="2" role="dialog" style="font-family: 'Trebuchet MS'; ">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header bg-info">
+                    <h4 class="modal-title" id=""> MNE Class Result Page <i class="fa"></i></h4>
+                     <button type="button" class="close" data-dismiss="modal">
+                        <span aria-hidden="true">&times;</span><span class="sr-only">Close</span>    
+                    </button>
+                </div>
+                
+                 <div class="modal-body"> <!-- -->
+                        <div class="container">
+                            <div class="row col-12">
+                                <div class="col-4"> 
+                                 <table class="table table-xs">
+                                     <tbody> 
+                                     <tr> <td class="mnetabletd">Attendance: </td> <td class="mnetabletd2" id='Ctab1mne1'> </td> </tr>
+                                     <tr> <td  class="mnetabletd">Class work: </td> <td class="mnetabletd2" id='Ctab1mne2'> </td> </tr>
+                                     <tr> <td  class="mnetabletd">Home work: </td> <td class="mnetabletd2" id='Ctab1mne3'> </td> </tr>
+                                     <tr> <td  class="mnetabletd">Tests: </td> <td class="mnetabletd2" id='Ctab1mne4'> </td> </tr>
+                                     <tr> <td  class="mnetabletd">Mid-Term: </td> <td class="mnetabletd2" id='Ctab1mne5'> </td> </tr>
+                                     <tr> <td  class="mnetabletd">Terminal: </td> <td class="mnetabletd2" id='Ctab1mne6'> </td> </tr>
+                                     
+                                     </tbody>
+                                </table>         
+                                </div>
+                                <div class="col-4"> 
+                                <table class="table table-xs">
+                                     <tbody> 
+                                     <tr> <td class="mnetabletd">Class Name/ID: </td> <td class="mnetabletd2" id='Ctab2mne1'> </td> </tr>
+                                    
+                                     <tr> <td class="mnetabletd"> Date: </td> <td class="mnetabletd2"  id='Ctab2mne2'> </td> </tr>
+                                    
+                                     </tbody>
+                                </table>
+                                </div>
+                                
+                                <div class="col-4">
+                                 <table class="table table-xs">
+                                     <tbody> 
+                                     <tr> <td class="mnetabletd">Requester Name: </td> <td  class="mnetabletd2" id='Ctab3mne1'> </td> </tr>
+                                     <tr> <td class="mnetabletd">Date/Time : </td> <td  class="mnetabletd2" id='Ctab3mne2'> </td> </tr>
+                                     <tr> <td class="mnetabletd">Request Count: </td> <td  class="mnetabletd2" id='Ctab3mne3'> </td> </tr>
+                                    <tr> <td class="mnetabletd">Copy/Print Authority: </td> <td  class="mnetabletd2" id='Ctab3mne3'> </td> </tr>
+                                     </tbody>
+                                </table>    
+                                </div>
+                            </div>
+                            <div class="row col-12">
+                              
+                                <table class="table table-xs">
+                                     <tbody id="tabbody3"> 
+                                    
+                                     </tbody>
+                                </table>  
+                            
+                            </div>
+                        </div>          
+                 </div>
+                 
+                 <div class="modal-footer"><button type="button" class="btn btn-default btn-block" data-dismiss="modal">Close</button> </div>
+               
+            </div>
+                
+        </div>
+         
+    </div>
